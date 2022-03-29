ac489a31275533f075ccbdaa4c9a1912d0d49f24
Kai Hendry <hendry@iki.fi>
2022-03-29 12:34:17 +0800

    Profiler

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ E.g. `throw new createError.InternalServerError("something went wrong");`
       "message": "something went wrong",
       "stack": "InternalServerError: something went wrong\n    at baseHandler (/var/task/handler.js:14:9)\n    at runRequest (/var/task/node_modules/@middy/core/index.js:86:32)"
     }
+
+# Profiler
+
+https://github.com/serkan-ozal/middy-profiler
+
+<img src="https://s.natalian.org/2022-03-29/1648528442_1918x1047.png">
+
+Example https://s.natalian.org/2022-03-29/cpu_profile.cpuprofile
--- handler.js
+++ handler.js
@@ -3,6 +3,8 @@
 const middy = require("@middy/core");
 const httpErrorHandler = require("@schibsted/middy-error-handler");
 const createError = require("http-errors");
+const eventLoopTracer = require("middy-event-loop-tracer");
+const profiler = require("middy-profiler");
 
 const { LambdaLog } = require("lambda-log");
 const log = new LambdaLog({
@@ -10,10 +12,23 @@ const log = new LambdaLog({
 });
 
 const baseHandler = async (event) => {
-  log.info("starting up");
-  throw new createError.InternalServerError("something went wrong");
+  log.info("waiting a second", {
+    bucket: process.env.MIDDY_PROFILER_S3_BUCKET_NAME,
+  });
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  log.info("done waiting");
+  // return ok
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "ok",
+    }),
+  };
 };
 
-const handler = middy(baseHandler).use(httpErrorHandler({ logger: log }));
+const handler = middy(baseHandler)
+  .use(eventLoopTracer())
+  .use(profiler())
+  .use(httpErrorHandler({ logger: log }));
 
 module.exports = { handler };
